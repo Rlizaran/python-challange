26a16b0f566f6a7ba4bfbbb37e5a9f799e48791e
Rodrigo Lizaran <rlizaran97@gmail.com>
2021-08-31 13:51:56 -0700

    helping for test

---
+++ PyPoll/test.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+# main function
+
+# This path goes to the directory Resources and select the budget_data.csv file
+filepath = os.path.join('Resources', 'election_data.csv')
+with open(filepath, 'r') as file:
+        csv_reader = csv.reader(file, delimiter=',')
+        for row in csv_reader:
+            print(row)
